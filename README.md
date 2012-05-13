0406e81df2eed3b038a574de0db906815c234105
arkxu <fangzhou@arkxu.com>
2012-05-13 01:33:15 -0700

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-online_irb
-==========
+Browser based IRB
+=================
 
 A very simple Ruby Interpreter Online, based on Sinatra
+
+How to use
+----------
+	bundle install
+	
+	ruby onlineirb.rb
+	
+Then you would be able to access localhost:4567 from your browser.
+
+![screen shot](screenshot.png "Screen Shot")
---
+++ onlineirb.rb
@@ -0,0 +1,45 @@
+require 'sinatra'
+require 'stringio'
+require 'cgi'
+
+__binding__ = binding
+
+
+get '/run' do
+  sio = StringIO.new
+  
+  code = CGI.unescapeHTML(params[:code])
+
+  old_stdout, $stdout = $stdout, sio
+  if code[-1] == '\\'
+    CODES += code[0..-2] + "\n "
+    return nil
+  else
+    if defined? CODES
+      CODES += code
+    else
+      CODES = code
+    end
+  end
+  
+  begin
+    ret = __binding__.eval(CODES, "IRB", 0)
+  rescue Exception => e
+    ret = e.message
+    ret += e.backtrace.inspect
+  ensure
+    CODES.clear
+  end
+  
+  $stdout = old_stdout # restore stdout
+  if ret
+    ret = ret.inspect + "\n" + sio.string
+  else
+    ret = sio.string
+  end
+  ret
+end
+
+get '/' do
+  erb :index
+end
